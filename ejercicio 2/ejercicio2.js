e99520dc4fd9b1048b9fd8f839fbe683219c2e84
Edgar Caballero <ecaballeroh@unimuto.edu.co>
2020-10-30 12:19:08 -0500

    ejercicio 2

---
+++ ejercicio 2/ejercicio2.js
@@ -0,0 +1,14 @@
+let n1; 
+let n2;
+let suma=0;
+
+n1 = Number(prompt('Ingresar numero 1'));
+n2 = Number(prompt('Ingresar numero 2'));
+
+suma = n1 + n2;
+
+alert("suma es " +suma);
+
+document.write("suma es " +suma);
+
+console.log("suma es " +suma)
